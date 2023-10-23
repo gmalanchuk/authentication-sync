455dc04b2bbdc341b21d1db77ec2bd17374c1801
Grigory Malanchuk <grishamalanchuk@gmail.com>
2023-10-23 14:33:07 +0300

    pre-commit

--- src/main.py
+++ src/main.py
@@ -1,20 +1,17 @@
-from datetime import datetime
-
 from fastapi import FastAPI
 
 
-app = FastAPI(title="AuthenticationSync API", version='1.0.0')
+app = FastAPI(title="AuthenticationSync API", version="1.0.0")
 
 
-@app.get('/')
+@app.get("/")
 async def hella() -> str:
-    return 'hella'
+    return "hella"
 
 
 @app.on_event("startup")
 async def startup() -> None:
     """Executed before the server starts"""
 
-    start_time = datetime.now()
-
-    end_time = datetime.now()
+    # start_time = datetime.now()
+    # end_time = datetime.now()
