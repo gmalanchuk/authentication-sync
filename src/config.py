229622073f5df67a6fb9f24876125341eebda0b1
Grigory Malanchuk <grishamalanchuk@gmail.com>
2023-10-23 15:10:03 +0300

    docker

--- src/config.py
+++ src/config.py
@@ -7,6 +7,16 @@ class Settings(BaseSettings):
 
     LOGGING_LEVEL: str
 
+    DB_NAME: str
+    DB_USER: str
+    DB_PASS: str
+    DB_PORT: int
+    DB_HOST: str
+
+    @property
+    def SQLALCHEMY_URL(self) -> str:
+        return f"postgresql+asyncpg://{self.DB_USER}:{self.DB_PASS}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
+
     class Config:
         env_file = ".env"
 
