75569357b696b6dcbf312a58a67f85f56b4a34f0
Grigory Malanchuk <grishamalanchuk@gmail.com>
2023-10-24 10:39:44 +0300

    models

---
+++ src/database/models/user.py
@@ -0,0 +1,14 @@
+from sqlalchemy import String
+from sqlalchemy.orm import Mapped, mapped_column
+
+from src.database.models.base import Base
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    username: Mapped[str] = mapped_column(type_=String(16), nullable=False, unique=True)
+    email: Mapped[str] = mapped_column(nullable=False, unique=True)
+    name: Mapped[str] = mapped_column(type_=String(32))
+    hashed_password: Mapped[str] = mapped_column(type_=String(64), nullable=False)
+    is_verified: Mapped[bool] = mapped_column(nullable=False, default=False)
