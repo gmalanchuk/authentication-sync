8f4a5e33fe43e69698ab73fdbfb5ef7c06d8fd04
Grigory Malanchuk <grishamalanchuk@gmail.com>
2023-10-28 14:01:31 +0300

    transfer login to the auth service

--- src/api/routers/auth.py
+++ src/api/routers/auth.py
@@ -1,13 +1,9 @@
 from fastapi import APIRouter, Depends
-from sqlalchemy import select
 
 from src.api.schemas.auth.login import UserLoginRequestSchema
 from src.api.schemas.auth.registration import UserRegistrationRequestSchema, UserRegistrationResponseSchema
-from src.database.models import User
-from src.database.session import async_session
 from src.services.auth import AuthService
 from src.services.dependencies import get_auth_service
-from src.utils.hash_password import HashPassword
 
 
 auth_router = APIRouter(prefix="/v1/auth", tags=["Auth"])
@@ -21,16 +17,7 @@ async def registration(
 
 
 @auth_router.post(path="/login/")
-async def login(request_user: UserLoginRequestSchema) -> bool | None:
-    hash_password = HashPassword()
-
-    user_dict = request_user.model_dump()
-
-    async with async_session() as session:
-        q = select(User).where(User.email == user_dict["email"])
-        user_exist = (await session.execute(q)).scalars().first()
-        if user_exist:
-            res = hash_password.verify_hash(user_dict["password"], user_exist.hashed_password)
-            return res
-
-        return None
+async def login(
+    request_user: UserLoginRequestSchema, auth_service: AuthService = Depends(get_auth_service)
+) -> bool | None:
+    return await auth_service.login(request_user)
--- src/services/auth.py
+++ src/services/auth.py
@@ -1,3 +1,4 @@
+from src.api.schemas.auth.login import UserLoginRequestSchema
 from src.api.schemas.auth.registration import UserRegistrationRequestSchema, UserRegistrationResponseSchema
 from src.config import logger
 from src.repositories.auth import AuthRepository
@@ -25,3 +26,17 @@ class AuthService:
         logger.info(f"User registered: {user_dict['username']}")
 
         return UserRegistrationResponseSchema(**res)
+
+    async def login(self, user: UserLoginRequestSchema) -> bool | None:
+        user_dict = user.model_dump()
+
+        user_exist = await self.auth_repository.get_one(model_field="email", value=user_dict["email"])
+
+        if user_exist:
+            res = self.hash_password.verify_hash(user_dict["password"], user_exist.hashed_password)
+            return res
+        else:
+            # вызывать исключение, что пользователь с такой почтой или паролем не найден
+            pass
+
+        return None
