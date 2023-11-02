ef0e76936c17c3350f2df6e9434ebc65efde83df
Grigory Malanchuk <grishamalanchuk@gmail.com>
2023-11-02 12:28:20 +0200

    expiration in token

--- src/utils/jwt_token.py
+++ src/utils/jwt_token.py
@@ -1,4 +1,5 @@
 import base64
+import datetime
 import hashlib
 
 from src.config import settings
@@ -8,7 +9,16 @@ class JWTToken:
     @staticmethod
     async def create_token(user_id: dict) -> str:
         header = base64.b64encode(str({"alg": "HS256", "typ": "JWT"}).encode()).decode()
-        payload = base64.b64encode(str({"user_id": user_id}).encode()).decode()
+        payload = base64.b64encode(
+            str(
+                {
+                    "user_id": user_id,
+                    "expiration": (datetime.datetime.utcnow() + datetime.timedelta(seconds=900)).strftime(
+                        "%Y-%m-%d %H:%M:%S"
+                    ),
+                }
+            ).encode()
+        ).decode()
         signature = hashlib.sha256((header + payload + settings.JWT_SECRET_KEY).encode()).hexdigest()
 
         jwt_token = f"{header}.{payload}.{signature}"
