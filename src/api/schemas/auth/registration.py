f6d621444df2b155564273a5ac984ad33a392b61
Grigory Malanchuk <grishamalanchuk@gmail.com>
2023-11-11 20:20:06 +0200

    jwt token validator

--- src/api/schemas/auth/registration.py
+++ src/api/schemas/auth/registration.py
@@ -2,6 +2,7 @@ from typing import NoReturn
 
 from fastapi import HTTPException
 from pydantic import BaseModel, EmailStr, field_validator
+from starlette import status
 
 
 class UserRegistrationRequestSchema(BaseModel):
@@ -13,13 +14,17 @@ class UserRegistrationRequestSchema(BaseModel):
     @field_validator("username")
     def validate_username(cls, username: str) -> str | NoReturn:
         if len(username) > 16:
-            raise HTTPException(detail="Username must be less than 16 characters", status_code=422)
+            raise HTTPException(
+                detail="Username must be less than 16 characters", status_code=status.HTTP_422_UNPROCESSABLE_ENTITY
+            )
         return username
 
     @field_validator("name")
     def validate_name(cls, name: str) -> str | NoReturn:
         if len(name) > 16:
-            raise HTTPException(detail="Name must be less than 32 characters", status_code=422)
+            raise HTTPException(
+                detail="Name must be less than 32 characters", status_code=status.HTTP_422_UNPROCESSABLE_ENTITY
+            )
         return name
 
 
--- src/api/schemas/permission/check_permission.py
+++ src/api/schemas/permission/check_permission.py
@@ -1,5 +1,17 @@
-from pydantic import BaseModel
+from typing import NoReturn
+
+from fastapi import HTTPException
+from pydantic import BaseModel, field_validator
+from starlette import status
 
 
 class TokenSchema(BaseModel):
-    token: str  # TODO сделать валидацию, чтобы это был жвт токен, а не строка
+    token: str
+
+    @field_validator("token")
+    def validate_token(cls, token: str) -> str | NoReturn:
+        try:
+            _, _, _ = token.split(".")
+        except ValueError:
+            raise HTTPException(detail="Invalid JWT token format", status_code=status.HTTP_422_UNPROCESSABLE_ENTITY)
+        return token
