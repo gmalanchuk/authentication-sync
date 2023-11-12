2a0aa7198be100d66981917146d0fd7e9d842d15
Grigory Malanchuk <grishamalanchuk@gmail.com>
2023-11-12 21:06:54 +0200

    postgres port

--- src/api/routers/auth.py
+++ src/api/routers/auth.py
@@ -1,5 +1,3 @@
-from typing import NoReturn
-
 from fastapi import APIRouter, Depends
 from starlette.responses import JSONResponse
 
@@ -15,12 +13,12 @@ auth_router = APIRouter(prefix="/v1/auth", tags=["Auth"])
 @auth_router.post(path="/registration/", response_model=UserRegistrationResponseSchema)
 async def registration(
     request_user: UserRegistrationRequestSchema, auth_service: AuthService = Depends(get_auth_service)
-) -> JSONResponse | NoReturn:
+) -> JSONResponse:
     return await auth_service.registration(request_user)
 
 
 @auth_router.post(path="/login/", response_model=None)
 async def login(
     request_user: UserLoginRequestSchema, auth_service: AuthService = Depends(get_auth_service)
-) -> JSONResponse | NoReturn:
+) -> JSONResponse:
     return await auth_service.login(request_user)
--- src/api/routers/permission.py
+++ src/api/routers/permission.py
@@ -1,5 +1,3 @@
-from typing import NoReturn
-
 from fastapi import APIRouter, Depends
 from starlette.responses import JSONResponse
 
@@ -14,5 +12,5 @@ permission_router = APIRouter(prefix="/v1/permission", tags=["Permissions"])
 @permission_router.post(path="/check/")
 async def check_permission(
     request_token: TokenSchema, permission_service: PermissionService = Depends(get_permission_service)
-) -> JSONResponse | NoReturn:
+) -> JSONResponse:
     return await permission_service.check(request_token)
