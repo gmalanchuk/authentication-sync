5df52b65f8661d3ea7007557edab7bf766b60eec
Grigory Malanchuk <grishamalanchuk@gmail.com>
2023-12-25 17:32:37 +0200

    update proto file

--- src/services/permission.py
+++ src/services/permission.py
@@ -10,14 +10,12 @@ class PermissionService:
         self.exception = BaseExceptions(tag)
         self.jwt_token = JWTToken(tag)
 
-    async def get_user_info(
-        self, token_dict: dict
-    ) -> tuple[int, str, str, str, str,]:
+    async def get_user_info_by_token(self, token_dict: dict) -> tuple[int, str, str, str, str]:
         payload = await self.jwt_token.decode_token(token_dict["token"])
 
         user = await self.permission_repository.get_one(model_field="id", value=payload["user_id"])
 
-        if not user.is_verified:
+        if not user.is_verified:  # TODO ВЫНЕСТИ В STATICMETHOD
             return await self.exception.must_be_confirmed("email")
 
         return user.id, user.username, user.email, user.name, user.role.value
