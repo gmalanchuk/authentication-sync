0803b9a8b2efc207e34b8b5e9c44d7a820548e5a
Grigory Malanchuk <grishamalanchuk@gmail.com>
2024-01-07 15:05:05 +0200

    rename grpc servicer

--- src/grpc_services/user.py
+++ src/grpc_services/user.py
@@ -7,16 +7,16 @@ from src.services.user import UserGRPCService
 
 class User(UserServicer):
     def __init__(self) -> None:
-        self.permission_service = UserGRPCService()
+        self.user_service = UserGRPCService()
 
     async def CheckUserToken(self, request: UserRequestToken, context: grpc.aio.ServicerContext) -> UserResponse:
-        user_id, username, email, name, role = await self.permission_service.get_user_info_by_token(
+        user_id, username, email, name, role = await self.user_service.get_user_info_by_token(
             token_dict={"token": request.token}
         )
         return UserResponse(user_id=user_id, username=username, email=email, name=name, role=role)
 
     async def CheckUserID(self, request: UserRequestID, context: grpc.aio.ServicerContext) -> UserResponse:
-        user_id, username, email, name, role = await self.permission_service.get_user_info_by_id(
+        user_id, username, email, name, role = await self.user_service.get_user_info_by_id(
             user_dict={"user_id": request.user_id}
         )
         return UserResponse(user_id=user_id, username=username, email=email, name=name, role=role)
